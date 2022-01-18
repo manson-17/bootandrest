f94f1bd65428cf3af10328a2bf68f40d7c54d5a3
manson17 <cms86@bk.ru>
2022-01-18 15:14:36 +0300

    commit

--- src/main/java/com/example/bootandrest/controller/AdminController.java
+++ src/main/java/com/example/bootandrest/controller/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/")
 public class AdminController {
 
     @GetMapping()
--- src/main/java/com/example/bootandrest/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/bootandrest/security/handler/LoginSuccessHandler.java
@@ -18,11 +18,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/admin");
-        } else {
-            httpServletResponse.sendRedirect("/user");
-        }
+       // Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+            httpServletResponse.sendRedirect("/");
     }
 }
