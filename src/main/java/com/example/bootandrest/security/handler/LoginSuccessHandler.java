46a005b68a9925eeec97fddfe881f506909aa64b
manson17 <cms86@bk.ru>
2022-01-28 12:56:12 +0300

    commit

--- src/main/java/com/example/bootandrest/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/bootandrest/security/handler/LoginSuccessHandler.java
@@ -23,7 +23,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException, ServletException {
        // Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
     //  httpServletResponse.sendRedirect("/");
-        System.out.println("AuthenticationSuccessHandler invoked");
+       // System.out.println("AuthenticationSuccessHandler invoked");
         System.out.println("Authentication name: " + authentication.getName());
         System.out.println("Authentication name: " + authentication.getPrincipal());
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
